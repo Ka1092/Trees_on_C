b1af48f2e8b4dd2d366cb52278d67bd9c44a703e
SchmidACode <andrey2018richbirds@mail.ru>
2026-03-11 16:06:57 +0100

    Task 6 DONE

--- Tree.c
+++ Tree.c
@@ -1,28 +1,37 @@
+#include "Tree.h"
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "Tree.h"
-
 struct Node {
-    int data;
+    char data;
     struct Node *pLeft;
     struct Node *pRight;
 };
 
-Tree *insert(Tree *root, int data) {
-    if (root == NULL) {
-        Tree *node = malloc(sizeof(Tree));
-        node->data = data;
-        node->pLeft = NULL;
-        node->pRight = NULL;
-        return node;
+int division_by_zero = 0;
+
+Tree *create_node(char data) {
+    Tree *node = (Tree *)malloc(sizeof(Tree));
+    node->data = data;
+    node->pLeft = NULL;
+    node->pRight = NULL;
+    return node;
+}
+
+Tree* create_tree(char* str, int* pos) {
+    if (str[*pos] == '\0') {
+        return NULL;
     }
 
-    if (data < root->data) {
-        root->pLeft = insert(root->pLeft, data);
-    } else if (data > root->data) {
-        root->pRight = insert(root->pRight, data);
+    char c = str[*pos];
+    (*pos)++;
+
+    Tree* root = create_node(c);
+    if (c=='+'||c=='-'||c=='*'||c=='/') {
+        root->pLeft = create_tree(str, pos);
+        root->pRight = create_tree(str, pos);
     }
 
     return root;
@@ -34,14 +43,14 @@ void print_tree_postorder(Tree *root) {
     }
     print_tree_postorder(root->pLeft);
     print_tree_postorder(root->pRight);
-    printf("%d ", root->data);
+    printf("%c ", root->data);
 }
 
 void print_tree_preorder(Tree *root) {
     if (root == NULL) {
         return;
     }
-    printf("%d ", root->data);
+    printf("%c ", root->data);
     print_tree_preorder(root->pLeft);
     print_tree_preorder(root->pRight);
 }
@@ -86,4 +95,33 @@ int tree_peek_count(Tree *root, int level) {
         return 1;
     }
     return tree_peek_count(root->pLeft, level-1)+tree_peek_count(root->pRight, level-1);
+}
+
+int eval_forlmula_tree(Tree *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->data>='0' && root->data<='9') {
+        return root->data - '0';
+    }
+
+    int left_value = eval_forlmula_tree(root->pLeft);
+    int right_value = eval_forlmula_tree(root->pRight);
+
+    switch (root->data) {
+        case '+':
+            return left_value + right_value;
+        case '-':
+            return left_value - right_value;
+        case '*':
+            return left_value * right_value;
+        case '/':
+            if (right_value == 0) {
+                division_by_zero = 1;
+                return 0;
+            }
+            return left_value / right_value;
+            default:
+            return 0;
+    }
 }
